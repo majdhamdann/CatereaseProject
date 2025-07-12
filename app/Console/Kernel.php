f42007ce873524ab37cd6e3056e95363c922b77a
Majd Al-Hamdan <mom27071@gmail.com>
2025-07-12 23:49:24 +0300

    create

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,10 +10,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
-    }
+    protected function schedule(Schedule $schedule)
+{
+    $schedule->call(function () {
+        \App\Models\Coupon::where('expiration_date', '<', now())->delete();
+    })->daily(); 
+}
+
 
     /**
      * Register the commands for the application.
