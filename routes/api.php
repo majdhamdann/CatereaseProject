7b8e71179ac537c87bba9b5e2a00a05bee554b09
Raghad Abd <raghad.hn.2002@gmail.com>
2025-07-15 01:36:34 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -52,12 +52,12 @@ Route::prefix('branches')->group(function () {
     Route::get('/nearby', [BranchController::class, 'getNearby']);
 });
 
-//Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/branches/{branch}/packages', [PackageController::class, 'index']);
     Route::get('packages/{id}', [PackageController::class, 'show']);
     Route::get('/packages', [PackageController::class, 'listPackages']);
 
-//});
+});
 Route::prefix('cart')->middleware('auth:sanctum')->group(function () {
 
     Route::post('/add', [CartController::class, 'addToCart']);
