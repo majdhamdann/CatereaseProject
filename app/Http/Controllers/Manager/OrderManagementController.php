fdfe77b04e142c0cc1c0d5de297326df3b5cf824
Majd Al-Hamdan <mom27071@gmail.com>
2025-08-29 15:20:52 +0300

    z

--- app/Http/Controllers/Manager/OrderManagementController.php
+++ app/Http/Controllers/Manager/OrderManagementController.php
@@ -76,6 +76,15 @@ class OrderManagementController extends Controller
         'amount' => $order->total_price,
         'issued_at' => now(),
     ]);
+            // إرسال إشعار للزبون
+        if ($order->user && $order->user->device_token) {
+           $this->unicast((object)[
+             'title' => 'Your order has been approved ',
+             'body'  => "Your order #{$order->id} has been confirmed and is being prepared."
+           ], $order->user->device_token);
+}
+
+
     return response()->json(['message' => 'Order approved successfully']);
     }
 
@@ -97,6 +106,13 @@ class OrderManagementController extends Controller
     $order->approved_by = $manager->id;
     $order->approved_at = now();
     $order->save();
+        if ($order->user && $order->user->device_token) {
+              $this->unicast((object)[
+                'title' => 'Your order was rejected ',
+                'body'  => "Your order #{$order->id} has been rejected. Reason: {$request->rejection_reason}"
+    ], $order->user->device_token);
+}
+
 
     return response()->json(['message' => 'Order rejected']);
     }
